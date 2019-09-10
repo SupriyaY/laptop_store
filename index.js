3f8623724a2661e4a3157d5a77bb44e1f27722b3
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-09-10 19:30:20 -0400

    routing products page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //core node modules
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //have to pass the absolute value to that file
 //need the UTF-8 because it will return something called a buffer instead of a file
@@ -13,9 +14,23 @@ console.log(laptopData);
 //run each time the webserver is accessed
 //request and response - objects with methods and properties that we are able to use
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end('this is the response')
-console.log("Some accessed the server!")
+
+    //query is parsed into an object
+    const pathName = url.parse(req.url, true).pathname;
+
+
+    if(pathName === '/products' || pathName === ""){
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end('this is the PRODUCTS PAGE'); 
+        } 
+        
+        else if(pathName === '/laptop'){
+            res.writeHead(200, {'Content-type': 'text/html'})
+            res.end('this is the LAPTOP PAGE'); 
+        }
+        
+
+
 });
 
 
