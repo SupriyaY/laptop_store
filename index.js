db9fd127acc4bdcbebbff222d449187c11c885e1
supriya  yerramilli <supriyayerramilli@supriyas-air.pyramidci.com>
2019-10-09 13:04:33 -0400

    added store data

--- index.js
+++ index.js
@@ -17,18 +17,35 @@ const server = http.createServer((req, res) => {
 
     //query is parsed into an object
     const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
 
-
-    if(pathName === '/products' || pathName === ""){
+    if(pathName === '/products' || pathName === "/"){
         res.writeHead(200, {'Content-type': 'text/html'})
         res.end('this is the PRODUCTS PAGE'); 
         } 
         
-        else if(pathName === '/laptop'){
+        else if(pathName === '/laptop' && id < laptopData.length){
             res.writeHead(200, {'Content-type': 'text/html'})
-            res.end('this is the LAPTOP PAGE'); 
+            // res.end(`this is the LAPTOP PAGE ${id}!`); 
+            fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+                const laptop = laptopData[id];
+                 output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+                 output = output.replace(/{%IMAGE%}/g, laptop.image);
+                 output = output.replace(/{%PRICE%}/g, laptop.price);
+                 output = output.replace(/{%SCREEN%}/g, laptop.screen);
+                 output = output.replace(/{%CPU%}/g, laptop.cpu);
+                 output = output.replace(/{%STORAGE%}/g, laptop.storage);
+                 output = output.replace(/{%RAM%}/g, laptop.ram);
+                 output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+
+            res.end(output);
+        });
         }
         
+        else{
+            res.writeHead(404, {'Content-type': 'text/html'})
+            res.end('URL was not found on the server'); 
+        }
 
 
 });
