0a843e754b60a8a6666a9dbdae5ba355ade418c2
supriya  yerramilli <supriyayerramilli@supriyas-air.pyramidci.com>
2019-10-09 16:52:09 -0400

    laptop id updates with url change

--- index.js
+++ index.js
@@ -21,27 +21,23 @@ const server = http.createServer((req, res) => {
 
     if(pathName === '/products' || pathName === "/"){
         res.writeHead(200, {'Content-type': 'text/html'})
-        res.end('this is the PRODUCTS PAGE'); 
+        // res.end('this is the PRODUCTS PAGE'); 
+
+
         } 
         
         else if(pathName === '/laptop' && id < laptopData.length){
             res.writeHead(200, {'Content-type': 'text/html'})
             // res.end(`this is the LAPTOP PAGE ${id}!`); 
             fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
-                const laptop = laptopData[id];
-                 output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-                 output = output.replace(/{%IMAGE%}/g, laptop.image);
-                 output = output.replace(/{%PRICE%}/g, laptop.price);
-                 output = output.replace(/{%SCREEN%}/g, laptop.screen);
-                 output = output.replace(/{%CPU%}/g, laptop.cpu);
-                 output = output.replace(/{%STORAGE%}/g, laptop.storage);
-                 output = output.replace(/{%RAM%}/g, laptop.ram);
-                 output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            const laptop = laptopData[id];
+             const output = replaceTemplate(data, laptop);
 
             res.end(output);
         });
         }
         
+
         else{
             res.writeHead(404, {'Content-type': 'text/html'})
             res.end('URL was not found on the server'); 
@@ -53,15 +49,23 @@ const server = http.createServer((req, res) => {
 
 
 
-
-
-
-
-
-
-
 //listening..
 //will just keep loading until you send results back to the browswer
 server.listen(1337,'127.0.0.1', () =>{
 console.log("Listening for the request now");
 });
+
+
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+}
